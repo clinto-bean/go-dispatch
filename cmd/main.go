a9b05bf90476e4e30e6b4a6391f85cc4d7fc2196
Clinto Bean <clintonclark1995@hotmail.com>
2024-07-13 10:25:24 -0500

    implement execution of commands via 'do [command]'

--- cmd/main.go
+++ cmd/main.go
@@ -8,13 +8,16 @@ import (
 )
 
 func main() {
+	fmt.Println()
 	fmt.Println("Welcome to go-dispatch! This lightweight worker will assist you with dispatching system commands and http handlers.")
 	fmt.Println("A list of commands can be found below.")
-	fmt.Println("If there are any performance concerns, please contact the maintainer.")
+	fmt.Println("If there are any concerns, please contact the maintainer.")
 	err := io.RunIORepl()
 	switch err {
-		case 1: fmt.Println("KILL: Exiting")
-		default: fmt.Printf("Uncaught exception, code %v\n", err)
+	case 1:
+		fmt.Println("EXIT: Exiting")
+	default:
+		fmt.Printf("Uncaught exception, code %v\n", err)
 	}
 	os.Exit(err)
 }
